bbec7df437f3758669cc7282de6ca9d6946e07ce
em <eugen.mayer@kontextwork.de>
2025-12-25 22:41:40 +0100

    add pgrestore

---
+++ prestore.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+FILE=$1
+DBNAME=$2
+
+if [ -z "$FILE" ]
+then
+      echo "Error: Please pass the file to import first paramter"
+      exit 1
+fi
+
+if [ -z "FILE" ]
+then
+      echo "Error: Please pass the target database name as 2nd paramter"
+      exit 1
+fi
+
+source .env
+NETWORK=${COMPOSE_PROJECT}_dbs
+docker run --rm --link postgres:$PG_DOMAIN -v $FILE:/tmp/dumpsql -e PGREQUIRESSL=1 -e PGSSLMODE=require -e PGPASSWORD=${POSTGRES_PASSWORD} -it --network $NETWORK postgres:latest pg_restore -h $PG_DOMAIN -U postgres $DBNAME --clean -f /tmp/dumpsql
