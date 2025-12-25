531b511c3814b4fa63f017348c1d79df9d848f84
em <eugen.mayer@kontextwork.de>
2025-12-25 14:47:44 +0100

    fix deploy pg

--- pg/certbot/deploy_pg.sh
+++ pg/certbot/deploy_pg.sh
@@ -7,5 +7,5 @@ cp -Lr /etc/letsencrypt/live/${PG_DOMAIN}/cert.pem /pg_certificates/server.crt
 cp -Lr /etc/letsencrypt/live/${PG_DOMAIN}/privkey.pem /pg_certificates/server.key
 cp -Lr /etc/letsencrypt/live/${PG_DOMAIN}/chain.pem /pg_certificates/chain.crt
 cp -Lr /etc/letsencrypt/live/${PG_DOMAIN}/fullchain.pem /pg_certificates/fullchain.crt
-chown 1001:1001 /pg_certificates -R
+chown 999:999 /pg_certificates -R
 chmod 770 /pg_certificates/
