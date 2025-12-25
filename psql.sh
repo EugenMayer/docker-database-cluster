dd384bf72a67a9ea5a8ab36a5e64dfb95de40ee1
em <eugen.mayer@kontextwork.de>
2025-12-25 22:32:05 +0100

    fix psql

--- psql.sh
+++ psql.sh
@@ -2,4 +2,4 @@
 set -e
 source .env
 NETWORK=${COMPOSE_PROJECT}_dbs
-docker run --rm --link postgres:$PG_DOMAIN -e PGSSLROOTCERT=/etc/ssl/certs/ca-certificates.crt -e PGREQUIRESSL=1 -e PGSSLMODE=verify-full -e PGPASSWORD=${POSTGRES_PASSWORD} -it --network $NETWORK postgre:latest psql -h $PG_DOMAIN -U postgres
+docker run --rm --link postgres:$PG_DOMAIN -e PGREQUIRESSL=1 -e PGSSLMODE=require -e PGPASSWORD=${POSTGRES_PASSWORD} -it --network $NETWORK postgres:latest psql -h $PG_DOMAIN -U postgres
